a94de0e5a83b6a43d1fb2216e04ca989f24e0af2
Taqiy-m <muhammadyunus.tursunov@gmail.com>
2024-04-06 18:13:21 +0500

    uchinchi kommit

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
-
-
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QPushButton, QLabel, QLineEdit, QTextEdit, QApplication
+
+
 ilova = QApplication([])
-a = QPushButton("Meni Bos!")
-a.setFixedSize(600, 600)
-b = QLabel("Bu shunchaki oddiy yozuv!!")
-b.setFixedSize(400, 300)
-a.show()
-b.show()
+
+lbl = QPushButton("Bu Shunchaki Yozuv!!")
+# lbl.setStyleSheet("""
+#                         QPushButton {
+#                             background-color: red;
+#                             font-size: 40px;
+#                         }
+#
+#                         QPushButton:hover {
+#                             background-color: black;
+#                             font-size: 50px;
+#                             color: white;
+#                         }
+#
+#                         QPushButton:pressed {
+#                             background-color: white;
+#                             color: black;
+#                             font-size: 50px;
+#                         }
+# """)
+
+lbl.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignBottom)
+lbl.show()
 ilova.exec()
+
+
+
+
+
